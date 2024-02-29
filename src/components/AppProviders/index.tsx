28e7af1c2ddb6183862111600a4a4d68904df01c
Gleb Elenev <g.elenev@gmail.com>
2024-02-29 06:40:24 +0300

    chore: react query

--- src/components/AppProviders/index.tsx
+++ src/components/AppProviders/index.tsx
@@ -1,4 +1,6 @@
-import { ReactNode } from 'react';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+import { ReactNode, useState } from 'react';
 
 import { ThemeProvider, theme } from '@scripts/gds';
 
@@ -7,7 +9,27 @@ interface AppProvidersProps {
 }
 
 const AppProviders = ({ children }: AppProvidersProps) => {
-    return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
+    const [queryClient] = useState(
+        () =>
+            new QueryClient({
+                defaultOptions: {
+                    queries: {
+                        staleTime: 60 * 5 * 1000,
+                        retry: 0,
+                        refetchOnWindowFocus: process.env.NODE_ENV === 'production',
+                    },
+                },
+            })
+    );
+
+    return (
+        <ThemeProvider theme={theme}>
+            <QueryClientProvider client={queryClient}>
+                {children}
+                <ReactQueryDevtools initialIsOpen={false} position="bottom-right" />
+            </QueryClientProvider>
+        </ThemeProvider>
+    );
 };
 
 export default AppProviders;
