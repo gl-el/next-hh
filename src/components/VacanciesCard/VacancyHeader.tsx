1acd4bb36e19c63ab18d94bede8d69a293d35e97
Gleb <g.elenev@gmail.com>
2024-03-20 15:19:38 +0300

    feat: responsive

--- src/components/VacanciesCard/VacancyHeader.tsx
+++ src/components/VacanciesCard/VacancyHeader.tsx
@@ -1,7 +1,7 @@
-import { scale } from '@greensight/gds';
+import { Layout, scale } from '@greensight/gds';
 import Image from 'next/image';
 
-import { Button, typography } from '@scripts/gds';
+import { Button, MEDIA_QUERIES, typography } from '@scripts/gds';
 
 interface VacancyHeaderProps {
     name: string;
@@ -11,36 +11,46 @@ interface VacancyHeaderProps {
 
 export default function VacancyHeader({ name, logo, responseUrl }: VacancyHeaderProps) {
     return (
-        <div
-            css={{
-                display: 'flex',
-                alignItems: 'start',
-                justifyContent: 'space-between',
-                gap: scale(3, true),
-                flexWrap: 'nowrap',
-            }}
-        >
-            <div css={{ display: 'flex', alignItems: 'start', justifyContent: 'flex-start', gap: scale(3, true) }}>
-                <h4 css={{ ...typography('h4') }}>{name}</h4>
-
-                {logo && (
-                    <Image
-                        src={logo}
-                        alt='Company logo'
-                        width={0}
-                        height={0}
-                        sizes="100vw"
-                        style={{
-                            width: '150px',
-                            height: '40px',
-                            objectFit: 'contain',
-                        }}
-                    />
-                )}
-            </div>
-            <Button as="a" theme="secondary" target="_blank" href={responseUrl} rel="noopener noreferrer">
-                Respond
-            </Button>
-        </div>
+        <Layout type="flex" align="start" justify="space-between" gap={scale(3, true)} wrap={{ xxxl: false, sm: true }}>
+            <Layout.Item>
+                <Layout
+                    type="flex"
+                    align="start"
+                    justify="start"
+                    gap={{ xxxl: scale(3, true), sm: scale(1) }}
+                    css={{
+                        flexDirection: 'row',
+                        [MEDIA_QUERIES.md]: {
+                            flexDirection: 'column-reverse',
+                        },
+                    }}
+                >
+                    <Layout.Item>
+                        <h4 css={{ ...typography('h4') }}>{name}</h4>
+                    </Layout.Item>
+                    {logo && (
+                        <Layout.Item>
+                            <Image
+                                src={logo}
+                                alt="Company logo"
+                                width={0}
+                                height={0}
+                                sizes="100vw"
+                                style={{
+                                    width: '150px',
+                                    height: '40px',
+                                    objectFit: 'contain',
+                                }}
+                            />
+                        </Layout.Item>
+                    )}
+                </Layout>
+            </Layout.Item>
+            <Layout.Item col={{ sm: '100%' }}>
+                <Button as="a" theme="secondary" target="_blank" href={responseUrl} rel="noopener noreferrer" block>
+                    Respond
+                </Button>
+            </Layout.Item>
+        </Layout>
     );
 }
